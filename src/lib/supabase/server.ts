fcf67447820559b46db87eb7b40372fa2a05f7b3
parthshinge <shingeparth@gmail.com>
2026-06-09 11:59:49 +0530

    fix: catch cookie modifications inside server components to avoid exceptions

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -18,10 +18,20 @@ export async function createSupabaseServerClient() {
           return cookieStore.get(name)?.value;
         },
         set(name, value, options) {
-          cookieStore.set({ name, value, ...options });
+          try {
+            cookieStore.set({ name, value, ...options });
+          } catch {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing user sessions.
+          }
         },
         remove(name, options) {
-          cookieStore.set({ name, value: "", ...options, maxAge: 0 });
+          try {
+            cookieStore.set({ name, value: "", ...options, maxAge: 0 });
+          } catch {
+            // The `remove` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing user sessions.
+          }
         },
       },
     },
